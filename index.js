01e0196211c5e2078332ad8b35cc2d16288478b7
Ankit <ankitmeena9783226195@gmail.com>
2024-12-17 14:17:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,23 +7,27 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Endpoint to display secrets
+// Endpoint to display secrets and GitHub variable
 app.get("/", (req, res) => {
     const secrets = {
         apiKey: process.env.SECRET_API_KEY,
         dbPassword: process.env.SECRET_DB_PASSWORD,
     };
 
+    const customVariable = process.env.CUSTOM_VARIABLE || "Not defined";
+
     if (secrets.apiKey === "api.macbease.com") {
         res.json({
             message: "Secrets retrieved successfully.",
             status: "Success",
             secrets,
+            githubVariable: customVariable,
         });
     } else {
         res.status(401).json({
             message: "Unauthorized: Invalid SECRET_API_KEY",
             status: "Failure",
+            githubVariable: customVariable,
         });
     }
 });
