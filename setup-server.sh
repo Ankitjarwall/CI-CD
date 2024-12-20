98ef33de21aad1604e777975b0dcf8bc99f83c2b
Ankit <ankitmeena9783226195@gmail.com>
2024-12-20 11:44:10 +0530

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -20,27 +20,29 @@ sudo apt install certbot python3-certbot-nginx -y
 echo "Installing Nginx..."
 sudo apt install nginx -y
 
-# Configure Nginx for Port Forwarding (5050 -> 80)
-echo "Configuring Nginx for port forwarding..."
-sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
-server {
-    listen 80;
-    server_name _;
+source ~/.bashrc
 
-    location / {
-        proxy_pass http://localhost:3000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
-    }
-}
-EOF'
+# Configure Nginx for Port Forwarding (5050 -> 80)
+#echo "Configuring Nginx for port forwarding..."
+#sudo bash -c 'cat > /etc/nginx/sites-available/default <<EOF
+#server {
+ #   listen 80;
+  #  server_name macbease.com admin.macbease.com doc.macbease.com backend.macbease.com;
+#
+ #   location / {
+ #       proxy_pass http://localhost:3000;
+ #      proxy_http_version 1.1;
+ #       proxy_set_header Upgrade \$http_upgrade;
+ #       proxy_set_header Connection "upgrade";
+  #      proxy_set_header Host \$host;
+  #      proxy_cache_bypass \$http_upgrade;
+  #  }
+#}
+#EOF'
 
 # Test Nginx Configuration
-echo "Testing Nginx configuration..."
-sudo nginx -t
+#echo "Testing Nginx configuration..."
+#sudo nginx -t
 
 # Restart Nginx
 echo "Restarting Nginx..."
